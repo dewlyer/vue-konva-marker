46f24e461f8031072ef13e182261b6227b3cf385
dewlyer <dewlyer@gmail.com>
2019-08-08 11:18:44 +0800

    update

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import VueKonva from 'vue-konva'
 import App from './App'
 import store from './store/store'
 
-Vue.use(VueKonva);
+Vue.config.performance = true;
 
 Vue.config.productionTip = false;
 
+Vue.use(VueKonva);
+
 new Vue({
     store,
+    el: '#app',
     render: h => h(App)
-}).$mount('#app');
+});
