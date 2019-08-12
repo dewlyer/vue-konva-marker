2575015b94493626f38a529a596572c96473cc3b
dewlyer <liluak@yeah.net>
2019-08-12 09:59:54 +0800

    update

--- src/components/Marker.vue
+++ src/components/Marker.vue
@@ -1,8 +1,7 @@
 <template>
-    <v-stage class="marker-stage" ref="stage" :config="stage"
+    <v-stage class="marker-stage" ref="stage" :config="stageConfig"
              @mousedown="handleStageMouseDown" @mouseup="handleStageMouseUp"
-             @mousemove="handleStageMouseMove" @mouseout="handleStageMouseOut"
-             @dragstart="handleDragstart" @dragend="handleDragend">
+             @mousemove="handleStageMouseMove" @mouseout="handleStageMouseOut">
 
         <v-layer ref="backgroundLayer">
             <background-group :src="background"/>
@@ -11,11 +10,11 @@
         <v-layer ref="rectsLayer">
             <rects-group v-for="(item, index) in rectList"
                          :key="index" :index="index" :list="item"
-                         :selected="selectedRectName"/>
+                         :selected="rectListSelectName"/>
         </v-layer>
 
         <v-layer ref="drawLayer">
-            <v-rect :config="drawingRectWithStyle"></v-rect>
+            <v-rect :config="drawingRectConfig"></v-rect>
         </v-layer>
 
     </v-stage>
@@ -24,6 +23,57 @@
 <script>
     import BackgroundGroup from './Background'
     import RectsGroup from './Rects'
+
+    const RECT_LIST = [
+        [
+            {
+                name: 'recta1',
+                x: 120,
+                y: 120,
+                width: 100,
+                height: 100,
+            },
+            {
+                name: 'recta2',
+                x: 550,
+                y: 180,
+                width: 300,
+                height: 158,
+            }
+        ],
+        [
+            {
+                name: 'rectb1',
+                x: 120,
+                y: 220,
+                width: 100,
+                height: 100,
+            },
+            {
+                name: 'rectb2',
+                x: 550,
+                y: 180,
+                width: 300,
+                height: 158,
+            }
+        ],
+        [
+            {
+                name: 'rectc1',
+                x: 120,
+                y: 320,
+                width: 100,
+                height: 100,
+            },
+            {
+                name: 'rectc2',
+                x: 550,
+                y: 180,
+                width: 300,
+                height: 158,
+            }
+        ]
+    ];
 
     export default {
         name: 'paper-marker',
@@ -45,205 +95,143 @@
         },
         data() {
             return {
-                stage: {
+                stageConfig: {
                     width: 0,
                     height: 0,
                     draggable: true
                 },
-
-                rectList: [
-                    [
-                        {
-                            name: 'recta1',
-                            x: 120,
-                            y: 120,
-                            width: 100,
-                            height: 100,
-                        },
-                        {
-                            name: 'recta2',
-                            x: 550,
-                            y: 180,
-                            width: 300,
-                            height: 158,
-                        }
-                    ],
-                    [
-                        {
-                            name: 'rectb1',
-                            x: 120,
-                            y: 220,
-                            width: 100,
-                            height: 100,
-                        },
-                        {
-                            name: 'rectb2',
-                            x: 550,
-                            y: 180,
-                            width: 300,
-                            height: 158,
-                        }
-                    ],
-                    [
-                        {
-                            name: 'rectc1',
-                            x: 120,
-                            y: 320,
-                            width: 100,
-                            height: 100,
-                        },
-                        {
-                            name: 'rectc2',
-                            x: 550,
-                            y: 180,
-                            width: 300,
-                            height: 158,
-                        }
-                    ]
-                ],
-                selectedRectName: '',
-
+                rectList: RECT_LIST,
+                rectListSelectName: '',
                 drawingRect: {
-                    name: 'rectDrawing',
-                    x: 0,
-                    y: 0,
-                    width: 0,
-                    height: 0
-                },
-                drawingRectStyle: {
-                    opacity: 0.9,
-                    stroke: '#c00',
-                    strokeWidth: 1,
-                    dash: [5, 3]
-                },
-                drawingRectVisible: false,
-                drawingRectStart: null,
-                drawingRectEnd: null,
-
+                    base: {
+                        name: 'rectDrawing',
+                        x: 0,
+                        y: 0,
+                        width: 0,
+                        height: 0
+                    },
+                    style: {
+                        opacity: 0.9,
+                        stroke: '#c00',
+                        strokeWidth: 1,
+                        dash: [5, 3]
+                    },
+                    visible: false,
+                    start: null,
+                    end: null,
+                }
             };
         },
         computed: {
-            drawingRectWithStyle() {
-                return Object.assign({}, this.drawingRect, this.drawingRectStyle, {
-                    visible: this.drawingRectVisible,
-                });
+            drawingRectConfig() {
+                return {
+                    ...this.drawingRect.base,
+                    ...this.drawingRect.style,
+                    visible: this.drawingRect.visible
+                }
             }
         },
         methods: {
+            updateStageSize() {
+                this.stageConfig.width = window.innerWidth;
+                this.stageConfig.height = window.innerHeight;
+            },
             createNewRect(index) {
                 const i = index - 1;
-                this.rectList[i].push(Object.assign({}, this.drawingRect, {
+                const rect = Object.assign({}, this.drawingRect.base, {
                     name: `rect_${i}_${new Date().getTime()}`
-                }));
+                });
+                this.rectList[i].push(rect);
+            },
+            getAbsolutePosition(event) {
+                const stage = event.target.getStage();
+                return stage.getPointerPosition();
             },
             updateDrawingRect() {
-                if (!this.drawingRectStart || !this.drawingRectEnd) {
+                if (!this.drawingRect.start || !this.drawingRect.end) {
                     return;
                 }
 
                 let stage = this.$refs.stage.getStage();
                 let x = stage.x();
                 let y = stage.y();
-                let drawStart = this.drawingRectStart;
-                let drawEnd = this.drawingRectEnd;
-                Object.assign(this.drawingRect, {
+                let drawStart = this.drawingRect.start;
+                let drawEnd = this.drawingRect.end;
+                Object.assign(this.drawingRect.base, {
                     x: Math.min(drawStart.x, drawEnd.x) - x,
                     y: Math.min(drawStart.y, drawEnd.y) - y,
                     width: Math.abs(drawStart.x - drawEnd.x),
-                    height: Math.abs(drawStart.y - drawEnd.y),
+                    height: Math.abs(drawStart.y - drawEnd.y)
                 });
             },
-            getAbsolutePosition(event) {
-                const stage = event.target.getStage();
-                const pointer = stage.getPointerPosition();
-                // const {x, y} = stage.getAbsolutePosition();
-                // console.log(x, y)
-                // return {
-                //     x: pointer.x,
-                //     y: pointer.y
-                // };
-                return pointer;
-            },
             resetDrawingStatus() {
-                this.drawingRectStart = null;
-                this.drawingRectEnd = null;
-                this.drawingRect.x = 0;
-                this.drawingRect.y = 0;
-                this.drawingRect.width = 0;
-                this.drawingRect.height = 0;
+                this.drawingRect.start = null;
+                this.drawingRect.end = null;
+                this.drawingRect.base.x = 0;
+                this.drawingRect.base.y = 0;
+                this.drawingRect.base.width = 0;
+                this.drawingRect.base.height = 0;
             },
+
             startDrawingRect(event) {
-                this.drawingRectVisible = true;
-                this.drawingRectStart = this.getAbsolutePosition(event);
+                if (!this.drawing) {
+                    return;
+                }
+                this.drawingRect.visible = true;
+                this.drawingRect.start = this.getAbsolutePosition(event);
             },
             endDrawingRect() {
-                this.drawingRectVisible = false;
+                if (!this.drawing) {
+                    return;
+                }
+                this.drawingRect.visible = false;
                 this.createNewRect(this.drawing);
                 this.resetDrawingStatus();
                 this.$emit('drawend');
             },
             doDrawingRect(event) {
+                if (!this.drawing) {
+                    return;
+                }
                 if (event.target !== event.currentTarget) {
-                    this.drawingRectEnd = this.getAbsolutePosition(event);
+                    this.drawingRect.end = this.getAbsolutePosition(event);
                     this.updateDrawingRect();
                 }
             },
 
-            updateStageSize() {
-                this.stage.width = window.innerWidth;
-                this.stage.height = window.innerHeight;
-            },
             handleStageMouseDown(event) {
                 const target = event.target;
                 const parent = target.getParent();
                 const className = target.getClassName();
 
                 if (parent && parent.getClassName() !== 'Transformer') {
-                    this.selectedRectName = className === 'Rect' ? target.name() : '';
+                    this.rectListSelectName = className === 'Rect' ? target.name() : '';
                 }
 
                 if (className !== 'Image') {
                     return;
                 }
 
-                if (this.drawing) {
-                    this.startDrawingRect(event);
-                }
+                this.startDrawingRect(event);
             },
             handleStageMouseUp(event) {
-                if (this.drawing) {
-                    this.endDrawingRect(event);
-                }
+                this.endDrawingRect(event);
             },
             handleStageMouseMove(event) {
-                if (this.drawing) {
-                    this.doDrawingRect(event);
-                }
-                // const mousePos = this.$refs.stage.getStage().getPointerPosition();
-                // const x = mousePos.x;
-                // const y = mousePos.y;
-                // this.text.text = 'X: ' + x + ', Y: ' + y;
+                this.doDrawingRect(event);
             },
-            handleStageMouseOut() {
-                // this.text.text = 'Mouseout';
-            },
-            handleDragstart() {
-            },
-            handleDragend() {
-            }
+            handleStageMouseOut() {},
         },
         watch: {
             drawing(value) {
                 const container = this.$refs.stage.getStage().container();
                 container.style.cursor = value ? 'crosshair' : 'default';
-                this.stage.draggable = !value;
+                this.stageConfig.draggable = !value;
             }
         },
         created() {
-            this.updateStageSize();
-        },
-        mounted() {
             window.addEventListener('resize', this.updateStageSize);
+            this.updateStageSize();
         }
     };
 </script>
