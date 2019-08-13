8594fe15a71a7f0911541459be220bbb903e3db2
dewlyer <dewlyer@gmail.com>
2019-08-13 14:35:52 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <script>
     import Paper_A from './assets/paper_a.jpg'
-    import Paper_B from './assets/paper_b.jpg'
+    // import Paper_B from './assets/paper_b.jpg'
     import PaperMarker from './components/Marker'
     import PaperAction from './components/Action'
 
@@ -24,8 +24,8 @@
             }
         },
         created() {
-            this.background.push(Paper_A);
-            this.background.push(Paper_B);
+            const defaultBackground = [Paper_A/*, Paper_B*/];
+            this.handleBackgroundChange(defaultBackground);
         },
         methods: {
             handleDrawEnd() {
