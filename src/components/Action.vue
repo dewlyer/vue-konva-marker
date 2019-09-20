7e6543f25f9635ccbf617099b4f41cce32d1f351
dewlyer <dewlyer@gmail.com>
2019-09-20 09:50:06 +0800

    re-struct

--- src/components/Action.vue
+++ src/components/Action.vue
@@ -1,15 +1,15 @@
 <template lang="pug">
     .action-wrapper
-        .action-panel(:style='{width: actionPanelWidth + "px"}')
-            b-card(no-body v-for='index of 4' :key='index')
-                b-card-header(header-tag='header' v-b-toggle='"accordion-" + index') {{actionPanelTitle[index - 1]}}
-                b-collapse(accordion='my-accordion' :id='"accordion-" + index' :visible='index === 1')
+        .action-panel(:style='actionPanelStyle')
+            b-card(no-body v-for='(item, index) of actionPanel.groups' :key='index')
+                b-card-header(header-tag='header' v-b-toggle='getAccordionId(index)') {{ item.title }}
+                b-collapse(accordion='action-panel' :id='getAccordionId(index)' :visible='index === 0')
                     b-card-body
-                        template(v-if='index === 1')
+                        template(v-if='index === 0')
+                            b-card-text Some quick example text to build on the
+                        template(v-else-if='index === 1')
                             b-card-text Some quick example text to build on the
                         template(v-else-if='index === 2')
-                            b-card-text Some quick example text to build on the
-                        template(v-else-if='index === 3')
                             b-card-text Some quick example text to build on the
                         template(v-else)
                             b-card-text Some quick example text to build on the
@@ -27,9 +27,22 @@
     import {mapGetters} from 'vuex'
     import {STAGE_PADDING} from '../config'
 
+    const ACTION_GROUPS = [
+        {
+            title: '试卷信息'
+        },
+        {
+            title: '客观题'
+        },
+        {
+            title: '主观题'
+        },
+        {
+            title: '选做题'
+        },
+    ];
     const COLORS = ['绿', '蓝', '红'];
     const STAGE_SCALE = [0.1, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 2];
-    const ACTION_GROUP_TITLE = ['试卷信息','客观题','主观题','选做题'];
 
     // const OUTLINE_STYLE = {
     //     'border-color': '#333',
@@ -67,12 +80,14 @@
         },
         data() {
             return {
+                actionPanel: {
+                    width: STAGE_PADDING,
+                    groups: ACTION_GROUPS
+                },
                 paperFiles: null,
                 colors: COLORS,
                 stageScale: STAGE_SCALE,
-                currentScale: 1,
-                actionPanelWidth: STAGE_PADDING,
-                actionPanelTitle: ACTION_GROUP_TITLE
+                currentScale: 1
             }
         },
         computed: {
@@ -80,6 +95,9 @@
                 drawing: 'marker/drawing',
                 scale: 'marker/scale'
             }),
+            actionPanelStyle() {
+                return {width: this.actionPanel.width + 'px'};
+            },
             currentScaleIndex() {
                 return this.stageScale.indexOf(this.currentScale);
             }
@@ -95,6 +113,9 @@
             }
         },
         methods: {
+            getAccordionId(index) {
+                return 'accordion-' + index;
+            },
             handlePaperIndexChange(index) {
                 this.$emit('update:showIndex', index);
             },
